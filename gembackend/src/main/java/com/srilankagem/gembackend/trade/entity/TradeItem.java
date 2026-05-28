ef5b0991c4116e73a504b644f4c97ea62cf9799a
Pinitha Savidya <savidyabalasooriya617@gmail.com>
2026-05-28 22:39:37 +0530

    Add Trades

---
+++ gembackend/src/main/java/com/srilankagem/gembackend/trade/entity/TradeItem.java
@@ -0,0 +1,37 @@
+package com.srilankagem.gembackend.trade.entity;
+
+import com.srilankagem.gembackend.gem.models.GemStone;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "trade_items")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TradeItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "trade_id", nullable = false)
+    private Trade trade;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "gem_id" , nullable = false)
+    private GemStone stone;
+
+    @Column(nullable = false)
+    private Integer quantity;
+
+    @Column(nullable = false)
+    private BigDecimal unitPrice;
+}
